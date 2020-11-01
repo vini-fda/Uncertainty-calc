4e516e41a42e1af3b99f6bdacc13e92b48e58133
Vinicius Freitas de Almeida <vinifreitas.d.a@gmail.com>
2020-11-01 17:31:28 -0300

    Added valid classifiers

--- setup.py
+++ setup.py
@@ -15,8 +15,9 @@ setuptools.setup(
     url="https://github.com/vini-fda/u-calc",
     packages=setuptools.find_packages(),
     classifiers=[
+        "Topic :: Scientific/Engineering :: Physics",
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GPLv3 License",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
